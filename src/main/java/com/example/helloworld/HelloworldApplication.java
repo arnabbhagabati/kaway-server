7dd61359cc8c86f04a9f1700b7f4770e06f2cdc6
Arnab Bhagabati <arnab.bhagabati@gmail.com>
2023-05-25 11:14:45 +0530

    starting afresh

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -3,30 +3,51 @@ package com.example.helloworld;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HelloworldApplication {
 
-  @Value("${NAME:World}")
-  String name;
-
-  @RestController
-  class HelloworldController {
-    @GetMapping("/")
-    String hello() {
-      return "Hello " + name + "!";
-    }
-
-
-    @GetMapping("/default")
-    String getDefaultdata() {
-      return "Hello Arnab" + name + "!";
-    }
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(HelloworldApplication.class, args);
   }
+}
+
+@Component
+@Order(0)
+class AppCdsApplicationListener implements ApplicationListener<ApplicationReadyEvent> {
+  private final boolean appcds;
+  private final ApplicationContext ctx;
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  AppCdsApplicationListener(@Value("${appcds:false}") boolean appcds,
+      ApplicationContext ctx) {
+    this.appcds = appcds;
+    this.ctx = ctx;
+  }
+
+  @Override
+  public void onApplicationEvent(ApplicationReadyEvent event) {
+    if (appcds) {
+      restTemplate.getForEntity("http://localhost:8080/", String.class);
+      SpringApplication.exit(ctx, () -> 0);
+    }
+  }
+}
+
+@RestController
+class HelloController {
+
+  @GetMapping("/")
+  String hello() {
+    return "Hello World!";
+  }
+
 }
