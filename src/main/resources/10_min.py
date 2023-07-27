e8b93fcea99114591cbc57333144c2bd1e7a7f9b
Arnab Bhagabati <arnab.bhagabati@gmail.com>
2023-07-27 10:27:04 +0530

    fixing bse sec list api and mboum api key

--- src/main/resources/10_min.py
+++ src/main/resources/10_min.py
@@ -5,16 +5,17 @@ def make_http_call():
     # Replace the URL with the desired endpoint you want to call
     url = "https://kaway-server-n3ahptldka-as.a.run.app/secList/NASDAQ"
     
-    print("url is",url)
+    print("url is",url,flush=True)
 
     # Make the HTTP request
     response = requests.get(url)
 
     # Process the response as needed
     if response.status_code == 200:
-        print("HTTP call successful")
+        print("HTTP call successful", flush=True)
+        print("Response data:", response.text[:100],flush=True)
     else:
-        print("HTTP call failed")
+        print("HTTP call failed", flush=True)
 
 # Schedule the HTTP call to be made every 10 minutes
 #while True:
@@ -24,9 +25,11 @@ def make_http_call():
     
     
 if __name__ == "__main__":    
-    print('making hhtp call now 1')
     while True:
-        print('making hhtp call now')
+        print('making hhtp call now',flush=True)
+        print()
+        print()
+        print()
         make_http_call()
         time.sleep(600)  # Wait for 10 minutes
 	
